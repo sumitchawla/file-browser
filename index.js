2666859c65fe11d3d78a7b7c12f2ac42dd7d9e96
Yasin <yasinsapli@gmail.com>
2017-07-14 00:00:09 +0300

    Adding -include option and moving to yargs from commander package for argument parsing

--- index.js
+++ index.js
@@ -7,7 +7,32 @@ var fs = require('fs');
 var path = require('path');
 var util = require('util');
 
-var program = require('commander');
+var argv = require('yargs')
+  .usage('Usage: $0 <command> [options]')
+  .command('$0', 'Browse file system.')
+  .example('$0 -e .js .swf .apk', 'Exclude extensions while browsing.')
+  .alias('i', 'include')
+  .array('i')
+  .describe('i', 'File extension to include.')
+  .alias('e', 'exclude')
+  .array('e')
+  .describe('e', 'File extensions to exclude.')
+  .alias('p', 'port')
+  .describe('p', 'Port to run the file-browser. [default:8088]')
+  .help('h')
+  .alias('h', 'help')
+  // .describe('h', '')
+  // .epilog('copyright 2015')
+  // .demandOption(['i', 'e']) // required fields
+  .check(_checkValidity)
+  .argv;
+
+function _checkValidity(argv) {
+  if (argv.i && argv.e) return new Error('Select -i or -e.');
+  if (argv.i && argv.i.length == 0) return new Error('Supply at least one extension for -i option.');
+  if (argv.e && argv.e.length == 0) return new Error('Supply at least one extension for -e option.');
+  return true;
+}
 
 function collect(val, memo) {
   if(val && val.indexOf('.') != 0) val = "." + val;
@@ -15,21 +40,16 @@ function collect(val, memo) {
   return memo;
 }
 
-program
-  .option('-p, --port <port>', 'Port to run the file-browser. Default value is 8088')
-  .option('-e, --exclude <exclude>', 'File extensions to exclude. To exclude multiple extension pass -e multiple times. e.g. ( -e .js -e .cs -e .swp) ', collect, [])
-  .parse(process.argv);
-
 var app = express();
 var dir =  process.cwd();
 app.use(express.static(dir)); //app public directory
 app.use(express.static(__dirname)); //module directory
 var server = http.createServer(app);
 
-if(!program.port) program.port = 8088;
+if(!argv.port) argv.port = 8088;
 
-server.listen(program.port);
-console.log("Please open the link in your browser http://<YOUR-IP>:" + program.port);
+server.listen(argv.port);
+console.log("Please open the link in your browser http://localhost:" + argv.port);
 
 app.get('/files', function(req, res) {
  var currentDir =  dir;
@@ -44,7 +64,8 @@ app.get('/files', function(req, res) {
       files
       .filter(function (file) {
           return true;
-      }).forEach(function (file) {
+      })
+      .forEach(function (file) {
         try {
                 //console.log("processing ", file);
                 var isDirectory = fs.statSync(path.join(currentDir,file)).isDirectory();
@@ -52,17 +73,21 @@ app.get('/files', function(req, res) {
                   data.push({ Name : file, IsDirectory: true, Path : path.join(query, file)  });
                 } else {
                   var ext = path.extname(file);
-                  if(program.exclude && _.contains(program.exclude, ext)) {
+                  if(argv.exclude && _.contains(argv.exclude, ext)) {
                     console.log("excluding file ", file);
                     return;
-                  }       
+                  }
+                  else if(argv.include && !_.contains(argv.include, ext)) {
+                    console.log("not including file", file);
+                    return;
+                  }
                   data.push({ Name : file, Ext : ext, IsDirectory: false, Path : path.join(query, file) });
                 }
 
         } catch(e) {
-          console.log(e); 
-        }        
-        
+          console.log(e);
+        }
+
       });
       data = _.sortBy(data, function(f) { return f.Name });
       res.json(data);
